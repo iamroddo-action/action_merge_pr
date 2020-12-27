323395b099dc7c738d47b6fb63b95acfb586841c
Rod Oliver <roderick.oliver@sap.com>
2020-12-27 08:38:24 +0100

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,10 +9,11 @@ set -u
 GH_HOST=$INPUT_HOST
 REPO=$(jq --raw-output .pull_request.base.repo.full_name $GITHUB_EVENT_PATH)
 GH_REPO="${GH_HOST}/${REPO}"
-PR_NUMBER=$(jq --raw-output .event.number $GITHUB_EVENT_PATH)
+PR_NUMBER=$(jq --raw-output .event $GITHUB_EVENT_PATH)
 
 echo "::debug::GH_HOST is '$GH_HOST'"
 echo "::debug::REPO_PATH is '$GH_REPO'"
 echo "::debug::PR_NUMBER is '$PR_NUMBER'"
+# gh pr merge $PR_NUMBER
 
 exit 0
