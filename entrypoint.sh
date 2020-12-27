61657b2be454d7f8f9e0372e40f155aa06960d27
Rod Oliver <roderick.oliver@sap.com>
2020-12-27 07:54:48 +0100

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,12 +9,7 @@ fi
 GH_HOST=$INPUT_HOST
 echo "::debug::The GH host is '$GH_HOST'"
 
-REPO=$(jq --raw-output .base.repo.full_name $GITHUB_EVENT_PATH)
+REPO=$(jq --raw-output .event.pull_request.base.repo.full_name $GITHUB_EVENT_PATH)
 echo "::debug::The GH repo is '$REPO'"
-
-# if [ "$GITHUB_EVENT_TYPE" != "opened" ] || [ "$GITHUB_EVENT_TYPE" != "reopened" ] ; then
-#   echo "::debug::The event type was '$GITHUB_EVENT_TYPE'"
-#   exit 0
-# fi
 
 exit 0
