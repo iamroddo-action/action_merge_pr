dbee6eb66c8e42398ac7f1d3eba3140613298930
Rod Oliver <roderick.oliver@sap.com>
2020-12-27 08:12:41 +0100

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,10 @@ if [ "$GITHUB_EVENT_NAME" != "pull_request" ]; then
 fi
 
 GH_HOST=$INPUT_HOST
+echo "::debug::GH_HOST is '$GH_HOST'"
 echo "::debug::GITHUB_EVENT_PATH is '$GITHUB_EVENT_PATH'"
 
-REPO=$(jq --raw-output .action $GITHUB_EVENT_PATH)
-echo "::debug::The GH repo is '$REPO'"
+REPO=$(jq --raw-output . $GITHUB_EVENT_PATH)
+echo "::debug::The REPO is '$REPO'"
 
 exit 0
