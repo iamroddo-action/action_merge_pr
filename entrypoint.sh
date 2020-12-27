c9bd9339bb395a37ddc69c7d80a3c17b110bca23
Rod Oliver <roderick.oliver@sap.com>
2020-12-27 07:56:06 +0100

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ fi
 GH_HOST=$INPUT_HOST
 echo "::debug::The GH host is '$GH_HOST'"
 
-REPO=$(jq --raw-output .event.pull_request.base.repo.full_name $GITHUB_EVENT_PATH)
+REPO=$(jq --raw-output .event.pull_request.base $GITHUB_EVENT_PATH)
 echo "::debug::The GH repo is '$REPO'"
 
 exit 0
